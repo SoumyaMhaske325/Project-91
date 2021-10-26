cd4e1a93fd586f98d1c936eb6fe3265de8d47354
SoumyaMhaske325 <83540797+SoumyaMhaske325@users.noreply.github.com>
2021-10-26 18:48:56 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,19 @@
+function addUser()
+{
+    player1_name=document.getElementById("Player1_name_input").value;
+    player2_name=document.getElementById("Player2_name_input").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name); 
+
+    window.location="quiz_game_page.html";
+
+}
+
+function send()
+{
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actual_answer=parseInt(number1)*parseInt(number2);
+
+}
